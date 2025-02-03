3d272685dde0bc6af662633beb60ed01d8984e55
Nicolas Herisson <nicolas.herisson77@gmail.com>
2025-02-03 09:16:43 +0100

    complete answer and tag models

---
+++ app/models/v2/answer.js
@@ -0,0 +1,21 @@
+import sequelize from "../../database/sequelize.js";
+import { DataTypes, Model } from "sequelize";
+
+export default class Answer extends Model {};
+
+nswer.init(
+    {
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        question_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        is_valid: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }
+);
