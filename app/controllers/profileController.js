3de44956466a23892e0a3472726476e8953db4f0
Nicolas Herisson <nicolas.herisson77@gmail.com>
2025-02-07 16:42:39 +0100

    coms purged | error message on profile page

--- app/controllers/profileController.js
+++ app/controllers/profileController.js
@@ -5,7 +5,7 @@ import { User } from "../models/v2/associations.js";
 const profileController = {
     showProfilePage: async (req, res) => {
         try {
-          res.status(200).render('profile');
+          res.status(200).render('profile', {errorMessage: null});
         } catch (error) {
           errors[500](req,res, error);
         }
@@ -17,24 +17,24 @@ const profileController = {
 
           if (!firstname || !lastname || !email) 
             res.locals.error = "Veuillez remplir tous les champs";
-console.log("after fields validation");
+
           if (!EmailValidator.validate(email))
             res.locals.error = "Email invalide";
-console.log("after email validation | user id = "+req.session.user.id);
+
           const user = await User.findOne({where: {id: req.session.user.id}});
-          console.log("After user find | user = "+JSON.stringify(user, null, 2));
-          console.log("lastname = "+lastname);
+
           user.firstname = firstname;
           user.lastname = lastname;
           user.email = email;
           await user.save();
 
-          console.log("After user save | user = "+JSON.stringify(user, null, 2));
+
           req.session.user = user;
           res.locals.user = user;
 
 
-          res.status(200).render('profile');
+          res.status(200).render('profile', { errorMessage: res.locals.error });
+
         } catch (error) {
           errors[500](req,res, error);
         }
